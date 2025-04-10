5c999fc2a5282f51ccf345342dbedd7a80ade4b4
siirielli <siiri.lietu@gmail.com>
2025-04-10 18:17:08 +0200

    feat: add Review model with schema for user ratings and comments on books

---
+++ app/models/Review.ts
@@ -0,0 +1,36 @@
+import { Schema, model, Types, type InferSchemaType } from "mongoose";
+
+// Define the schema for the Review collection in MongoDB
+const reviewSchema = new Schema(
+  {
+    book: {
+      type: Types.ObjectId,
+      ref: "Book", // link to books collection
+      required: true,
+    },
+    user: {
+      type: Types.ObjectId,
+      ref: "User", // link to users collection
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1, // Minimum rating is 1
+      max: 5, // Maximum rating is 5
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }, // Automatically add `createdAt` and `updatedAt`
+);
+
+export type ReviewType = InferSchemaType<typeof reviewSchema> & {
+  _id: Types.ObjectId | string;
+};
+
+const Review = model<ReviewType>("Review", reviewSchema);
+
+export default Review;
